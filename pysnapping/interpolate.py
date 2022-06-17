5bb28f598683ae9be57f8f5ad01415587d3e74cc
Alexander Held | geOps <alexander.held@geops.de>
2022-06-17 17:47:18 +0200

    Add test for fixing repeated x

--- pysnapping/interpolate.py
+++ pysnapping/interpolate.py
@@ -5,14 +5,15 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 
-def fix_repeated_x(x: ArrayLike, y: ArrayLike, axis: int):
+def fix_repeated_x(x: ArrayLike, y: ArrayLike):
     """Remove repeated consecutive x values and average corresponding y values.
 
     `x` is assumed to be sorted in ascending order.
-    `axis` tells which axis of `y` corresponts to `x`.
+    Axis 0 of y is expected to correspond to x values.
     """
     x_arr = np.asarray(x, dtype=float)
     y_arr = np.asarray(y, dtype=float)
+    assert len(x_arr) == len(y_arr)
     n_points = len(x_arr)
 
     x_indices = [
@@ -23,18 +24,13 @@ def fix_repeated_x(x: ArrayLike, y: ArrayLike, axis: int):
         # speedup if nothing is repeated
         return x_arr, y_arr
     else:
-        xy = [
-            (x, y_arr.take(indices, axis=axis).mean(axis)) for x, indices in x_indices
-        ]
-
-        new_n_points = len(xy)
+        new_n_points = len(x_indices)
         new_x = np.empty(new_n_points)
         new_y_shape: typing.Any = list(y_arr.shape)
-        new_y_shape[axis] = new_n_points
+        new_y_shape[0] = new_n_points
         new_y_shape = tuple(new_y_shape)
         new_y = np.empty(new_y_shape)
-        # np.moveaxis gives a view of an array
-        # we move the target axis to the beginning since our calculated y values
-        # are shaped like this
-        new_x[...], np.moveaxis(new_y, axis, 0)[...] = zip(*xy)
+        new_x[...], new_y[...] = zip(
+            *((x, y_arr[indices].mean(0)) for x, indices in x_indices)
+        )
         return new_x, new_y
--- pysnapping/test/test_interpolate.py
+++ pysnapping/test/test_interpolate.py
@@ -1,39 +1,14 @@
 from numpy.testing import assert_allclose
 import numpy as np
-import pytest
+
+from pysnapping.interpolate import fix_repeated_x
 
 
-from pysnapping.interpolate import interp1d_assume_sorted_allow_repeated as interp
-
-
-def test_interpolate():
-    f = interp([0, 1, 1, 2], [[10, 1], [20, 2], [30, 3], [40, 4]], axis=0)
-    assert_allclose(f(1), [25, 2.5])
-
-    f = interp([0, 0], [1, 3])
-    assert_allclose(f(0), 2)
-    assert_allclose(f([0, 0]), [2, 2])
-    with pytest.raises(ValueError):
-        f(0.1)
-    with pytest.raises(ValueError):
-        f(-0.1)
-
-    f = interp([0, 0], [1, 3], bounds_error=False)
-    assert np.isnan(f(0.1))
-
-    f = interp([0, 0, 0], [2, 3, 4], bounds_error=False, fill_value="extrapolate")
-    assert_allclose(f(0), 3)
-    with pytest.raises(ValueError):
-        f(0.1)
-    with pytest.raises(ValueError):
-        f(-0.1)
-
-    f = interp([1, 1], [1, 1], fill_value=10, bounds_error=False)
-    assert f(0.99) == 10
-    assert f(1.01) == 10
-    assert_allclose(f([0.99, 1.01]), [10, 10])
-
-    f = interp([1, 1], [1, 1], fill_value=(9, 10), bounds_error=False)
-    assert f(0.99) == 9
-    assert f(1.01) == 10
-    assert_allclose(f([1.01, 1, 0.99, np.nan]), [10, 1, 9, np.nan])
+def test_fix_repeated_x():
+    x = np.array([1, 2, 2, 3, 4, 4, 4], dtype=float)
+    y = np.array(
+        [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14]], dtype=float
+    )
+    fixed_x, fixed_y = fix_repeated_x(x, y)
+    assert_allclose(fixed_x, [1, 2, 3, 4])
+    assert_allclose(fixed_y, [[1, 2], [4, 5], [7, 8], [11, 12]])
